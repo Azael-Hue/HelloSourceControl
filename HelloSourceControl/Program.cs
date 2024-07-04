30eda8e2c98b6a0e38ed57ec03932665e850a371
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-04 11:39:35 -0700

    Added a greeting to the user

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitHub!");
 
+Console.Write("Type in your name: ");
+string ?userName = Console.ReadLine();
+Console.WriteLine($"Hello {userName}");
+
 for (int i = 0; i <= 10; i++)
 {
     Console.WriteLine($"Number: {i}");
